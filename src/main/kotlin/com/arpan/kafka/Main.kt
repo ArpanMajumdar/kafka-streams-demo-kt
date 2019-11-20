f3262b5ef1af3baf27cc0c80a462ee419e388f6e
Arpan Majumdar <arpan.majumdar.dev@gmail.com>
2019-11-20 08:28:55 +0530

    Completed favorite color app

--- src/main/kotlin/com/arpan/kafka/Main.kt
+++ src/main/kotlin/com/arpan/kafka/Main.kt
@@ -1,12 +1,13 @@
 package com.arpan.kafka
 
+import com.arpan.kafka.favoritecolor.FavoriteColor
 import com.arpan.kafka.wordcount.WordCount
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsConfig
 
 fun main() {
-    runWordCountApp()
+    runFavoriteColorApp()
 }
 
 fun runWordCountApp() {
@@ -22,4 +23,20 @@ fun runWordCountApp() {
 
     val wordCount = WordCount(inputTopic, outputTopic, properties)
     wordCount.start()
+}
+
+fun runFavoriteColorApp() {
+    val inputTopic = "favorite-color-input"
+    val outPutTopic = "favorite-color-output"
+    val intermediateTopic = "favorite-color-compacted"
+
+    val properties = mapOf(
+        StreamsConfig.APPLICATION_ID_CONFIG to "favorite-color-app",
+        StreamsConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+        StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG to Serdes.String()::class.java,
+        StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG to Serdes.String()::class.java
+    )
+    val favoriteColor = FavoriteColor(inputTopic, outPutTopic, intermediateTopic, properties)
+    favoriteColor.start()
 }
---
+++ src/main/kotlin/com/arpan/kafka/favoritecolor/FavoriteColor.kt
@@ -0,0 +1,63 @@
+package com.arpan.kafka.favoritecolor
+
+import org.apache.kafka.common.protocol.types.Field
+import org.apache.kafka.common.serialization.Serde
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.KeyValue
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.kstream.Produced
+import org.slf4j.LoggerFactory
+import java.util.*
+import java.util.concurrent.CountDownLatch
+import kotlin.system.exitProcess
+
+class FavoriteColor(
+    private val inputTopic: String,
+    private val outputTopic: String,
+    private val intermediateTopic: String,
+    private val props: Map<String, Any>
+) {
+    private val logger = LoggerFactory.getLogger(FavoriteColor::class.java)
+    private val properties = Properties().apply {
+        props.forEach { (key, value) -> put(key, value) }
+    }
+    private val streamsBuilder = StreamsBuilder()
+    private val source = streamsBuilder.stream<String, String>(inputTopic)
+    private val intermediate = streamsBuilder.table<String, String>(intermediateTopic)
+    private val colorsByUserId = source
+        .filter { _, value -> """\w+,\w+""".toRegex().containsMatchIn(value) }
+        .mapValues { _, value -> value.toLowerCase().split(",") }
+        .selectKey { _, value -> value[0] }
+        .mapValues { _, value -> value[1] }
+        .filter { _, color -> color in setOf("red", "green", "blue") }
+        .peek { user, color -> logger.info("$user -> $color") }
+    private val colorsGrouped = intermediate
+        .groupBy { _, color -> KeyValue(color, color) }
+        .count()
+
+    fun start() {
+        colorsByUserId.to(intermediateTopic, Produced.with(Serdes.String(), Serdes.String()))
+        colorsGrouped.toStream().to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()))
+
+        val kafkaStreams = KafkaStreams(streamsBuilder.build(), properties)
+
+        // Shutdown hook
+        val countDownLatch = CountDownLatch(1)
+        Runtime.getRuntime().addShutdownHook(Thread
+        {
+            kafkaStreams.close()
+            countDownLatch.countDown()
+        })
+
+        // Start kafka streams
+        try {
+            kafkaStreams.start()
+            countDownLatch.await()
+        } catch (ex: Exception) {
+            println("Error occurred")
+            println(ex.printStackTrace())
+            exitProcess(-1)
+        }
+    }
+}
